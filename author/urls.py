f0069dcd7932e1a860102ec48254826655fffe9a
alessandragaleno <alessandragaleno421@gmail.com>
2024-08-05 21:22:22 -0300

    arquivos .py atualizados

--- author/urls.py
+++ author/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import inicio, home
+from .views import inicio, home, index
 
 urlpatterns = [
     # arquivos html
     path('inicio/', inicio, name='inicio'),
     path('home/', home, name='home'),
+    path('index/',index, name='index'),
 ]
