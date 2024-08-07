88d2e03c394162b7d4cea422afcdc78e631300af
alessandragaleno <alessandragaleno421@gmail.com>
2024-08-07 18:33:33 -0300

    pasta statica adicionada

--- author/urls.py
+++ author/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import inicio, home, index
+from .views import inicio,home, index, contact, post
 
 urlpatterns = [
     # arquivos html
     path('inicio/', inicio, name='inicio'),
     path('home/', home, name='home'),
     path('index/',index, name='index'),
+    path('contact/', contact, name='contact'),
+    path('post/', post, name='post'),
+]
 ]
